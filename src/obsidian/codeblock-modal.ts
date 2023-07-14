7a47355bc4755267fa3426d5129c36e72a65b30f
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-07-14 17:31:28 -0600

    feat: add modals

---
+++ src/obsidian/codeblock-modal.ts
@@ -0,0 +1,50 @@
+import { App, Modal } from "obsidian";
+
+type SaveCallback = (value: string) => void;
+
+export default class LinkModal extends Modal {
+	inputEl?: HTMLTextAreaElement;
+	onSave: SaveCallback;
+
+	constructor(app: App, onSave: SaveCallback) {
+		super(app);
+		this.onSave = onSave;
+	}
+
+	onOpen(): void {
+		const { contentEl } = this;
+		contentEl.createEl("h4", { text: "Add codeblock" });
+
+		const containerEl = contentEl.createDiv();
+		containerEl.style.display = "flex";
+		containerEl.style.flexDirection = "column";
+		containerEl.style.gap = "var(--size-4-4)";
+
+		this.renderInputEl(containerEl);
+		this.renderButtonEl(containerEl);
+	}
+
+	onClose() {
+		this.contentEl.empty();
+	}
+
+	private renderInputEl(containerEl: HTMLElement) {
+		this.inputEl = containerEl.createEl("textarea");
+		this.inputEl.style.height = "150px";
+		this.inputEl.style.resize = "none";
+		this.inputEl.focus();
+	}
+
+	private renderButtonEl(containerEl: HTMLElement) {
+		const buttonEl = containerEl.createEl("button", {
+			text: "Save",
+		});
+		buttonEl.addEventListener("click", () => {
+			const value = this.inputEl?.value;
+			if (value) {
+				this.onSave(value);
+				this.close();
+			}
+		});
+	}
+}
---
+++ src/obsidian/file-modal.ts
@@ -0,0 +1,31 @@
+import { App, FuzzySuggestModal, TFile } from "obsidian";
+
+type SaveCallback = (value: TFile) => void;
+
+export default class FileModal extends FuzzySuggestModal<TFile> {
+	onSaveCallback: SaveCallback;
+
+	constructor(app: App, onSaveCallback: SaveCallback) {
+		super(app);
+		this.onSaveCallback = onSaveCallback;
+	}
+
+	onOpen(): void {
+		const { contentEl } = this;
+	}
+
+	onClose() {
+		this.contentEl.empty();
+	}
+
+	getItems(): TFile[] {
+		return app.vault.getFiles();
+	}
+	getItemText(item: TFile): string {
+		return item.name;
+	}
+	onChooseItem(item: TFile, evt: MouseEvent | KeyboardEvent): void {
+		this.onSaveCallback(item);
+		this.close();
+	}
+}
