744a66da3576d99cc3d83cab0bacbeceb477f63e
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-07-14 17:33:07 -0600

    feat: add createContainer

--- src/data/app-state-factory.ts
+++ src/data/app-state-factory.ts
@@ -1,40 +1,28 @@
 import { CURRENT_PLUGIN_VERSION } from "./constants";
-import { AppState, ContainerType, LayoutOptions } from "./types";
-import { v4 as uuid } from "uuid";
+import {
+	AppState,
+	Container,
+	ContainerType,
+	LayoutOptions,
+} from "../shared/types";
 
 export const createDefaultAppState = (): AppState => {
 	return {
 		pluginVersion: CURRENT_PLUGIN_VERSION,
 		layout: LayoutOptions.TWO_BY_TWO,
-		data: [
-			{
-				id: uuid(),
-				position: 0,
-				type: ContainerType.LINK,
-				content: "",
-				isVisible: true,
-			},
-			{
-				id: uuid(),
-				position: 0,
-				type: ContainerType.LINK,
-				content: "",
-				isVisible: true,
-			},
-			{
-				id: uuid(),
-				position: 0,
-				type: ContainerType.LINK,
-				content: "",
-				isVisible: true,
-			},
-			{
-				id: uuid(),
-				position: 0,
-				type: ContainerType.LINK,
-				content: "",
-				isVisible: true,
-			},
-		],
+		data: [],
+	};
+};
+
+export const createContainer = (
+	type: ContainerType,
+	content: string
+): Container => {
+	return {
+		id: "",
+		position: 0,
+		type,
+		content,
+		isVisible: true,
 	};
 };
